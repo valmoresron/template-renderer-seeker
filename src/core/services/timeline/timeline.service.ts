ed198aaec0a530bffdacd8ac5fc9be4c3cfe3f18
Francis Daniel Valmores <francisdaniel.valmores@engagis.com>
2025-09-26 16:48:57 +0800

    set active slide on seek

--- src/core/services/timeline/timeline.service.ts
+++ src/core/services/timeline/timeline.service.ts
@@ -1,11 +1,17 @@
-import { effect, Injectable, signal } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import gsap from 'gsap';
 import { Subject } from 'rxjs';
+import { PlayerService } from '../player/player.service';
+import { PlayerStateService } from '../player/player-state.service';
+import { Slide } from 'src/core/models/renderer/slide/slide.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TimelineService {
+  private playerService = inject(PlayerService);
+  private playerState = inject(PlayerStateService);
+
   readonly gsapTick$ = new Subject<void>();
 
   readonly masterTimeline = signal<gsap.core.Timeline | null>(null);
@@ -23,7 +29,15 @@ export class TimelineService {
   }
 
   seek(seconds: number): void {
-    this.masterTimeline()?.seek(seconds);
+    const masterTimeline = this.masterTimeline();
+    if (masterTimeline) {
+      const activeSlideFromThisSeconds = this.getSlideFromDuration(seconds);
+      if (activeSlideFromThisSeconds) {
+        this.playerService.activeSlide.set(activeSlideFromThisSeconds);
+      }
+
+      masterTimeline.seek(seconds);
+    }
   }
 
   pause(): void {
@@ -34,5 +48,24 @@ export class TimelineService {
   play(): void {
     this.masterTimeline()?.play();
     this.paused.set(false);
+  }
+
+  private getSlideFromDuration(duration: number): Slide | null {
+    const slides = this.playerState.slides();
+
+    if (slides) {
+      let totalDuration = 0;
+      let prevSlide = slides[0];
+      for (const slide of slides) {
+        totalDuration += slide.properties.duration / 1000;
+        if (totalDuration > duration) {
+          break;
+        }
+      }
+
+      return prevSlide;
+    }
+
+    return null;
   }
 }
